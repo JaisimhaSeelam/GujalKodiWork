41d61af78c72b3b065c0972548703ab1e9a5e9b2
JaisimhaSeelam <jaisimhasn@gmail.com>
2026-03-06 22:42:23 -0500

    pshing more logs

--- zips/plugin.video.deccandelight/plugin.video.deccandelight-2.0.57/plugin.video.deccandelight/resources/scrapers/mrulz.py
+++ zips/plugin.video.deccandelight/plugin.video.deccandelight-2.0.57/plugin.video.deccandelight/resources/scrapers/mrulz.py
@@ -28,6 +28,8 @@ class mrulz(Scraper):
         Scraper.__init__(self)
         self.bu = 'https://www.5movierulz.claims/category/'
         self.icon = self.ipath + 'mrulz.png'
+        self.log('[MRULZ] __init__: Base URL set to: %s' % self.bu)
+        self.log('[MRULZ] __init__: Icon path: %s' % self.icon)
         self.list = {'01Tamil Movies': self.bu + 'tamil-movie/',
                      '02Telugu Movies': self.bu + 'telugu-movie/',
                      '03Malayalam Movies': self.bu + 'malayalam-movie/',
@@ -44,26 +46,35 @@ class mrulz(Scraper):
                      '99[COLOR yellow]** Search **[/COLOR]': self.bu[:-9] + '?s='}
 
     def get_menu(self):
+        self.log('[MRULZ] get_menu: Called, returning %d categories' % len(self.list))
         return (self.list, 7, self.icon)
 
     def get_items(self, url):
+        self.log('[MRULZ] get_items: Called with URL: %s' % url)
         movies = []
         if url[-3:] == '?s=':
             search_text = self.get_SearchQuery('Movie Rulz')
             search_text = urllib_parse.quote_plus(search_text)
             url = url + search_text
+            self.log('[MRULZ] get_items: Search query detected, updated URL: %s' % url)
 
+        self.log('[MRULZ] get_items: Making HTTP request...')
         html = client.request(url, headers=self.hdr)
-        self.log('[MRULZ] Requested URL: %s' % url)
-        self.log('[MRULZ] HTML response length: %d' % len(html) if html else 0)
+        self.log('[MRULZ] get_items: HTTP request completed')
+        self.log('[MRULZ] get_items: HTML response length: %d bytes' % len(html) if html else 'None')
         
+        if not html:
+            self.log('[MRULZ] get_items: ERROR - No HTML received from server!')
+            return (movies, 8)
+        
+        self.log('[MRULZ] get_items: Parsing HTML with BeautifulSoup...')
         mlink = SoupStrainer('div', {'id': 'content'})
         mdiv = BeautifulSoup(html, "html.parser", parse_only=mlink)
         plink = SoupStrainer('nav', {'id': 'posts-nav'})
         Paginator = BeautifulSoup(html, "html.parser", parse_only=plink)
         items = mdiv.find_all('div', {'class': 'boxed film'})
         
-        self.log('[MRULZ] Found %d items' % len(items))
+        self.log('[MRULZ] get_items: Found %d movie items' % len(items))
 
         for item in items:
             title = self.unescape(item.text)
@@ -88,26 +99,39 @@ class mrulz(Scraper):
         return (movies, 8)
 
     def get_videos(self, url):
+        self.log('[MRULZ] get_videos: Called with URL: %s' % url)
         videos = []
 
+        self.log('[MRULZ] get_videos: Making HTTP request...')
         html = client.request(url, headers=self.hdr)
+        self.log('[MRULZ] get_videos: HTTP request completed, response length: %d bytes' % len(html) if html else 'None')
+        
         mlink = SoupStrainer('div', {'class': 'entry-content'})
         videoclass = BeautifulSoup(html, "html.parser", parse_only=mlink)
 
         try:
             links = videoclass.find_all('a')
+            self.log('[MRULZ] get_videos: Found %d <a> tags in entry-content' % len(links))
             for link in links:
                 vidurl = link.get('href')
+                self.log('[MRULZ] get_videos: Processing link: %s' % vidurl)
                 self.resolve_media(vidurl, videos)
-        except:
+        except Exception as e:
+            self.log('[MRULZ] get_videos: Exception in <a> tag processing: %s' % str(e))
             pass
 
         r = re.search(r'var\s*locations\s*=\s*([^;]+)', html)
         if r:
+            self.log('[MRULZ] get_videos: Found locations variable in HTML')
             links = json.loads(r.group(1))
+            self.log('[MRULZ] get_videos: Parsed %d links from locations variable' % len(links))
             for link in links:
                 if 'vcdnlare' in link:
                     link += '$${0}'.format(url)
+                self.log('[MRULZ] get_videos: Processing location link: %s' % link)
                 self.resolve_media(link, videos)
-
+        else:
+            self.log('[MRULZ] get_videos: No locations variable found in HTML')
+        
+        self.log('[MRULZ] get_videos: Total videos found: %d' % len(videos))
         return videos
